d1fbd367756d19b62ed3e01d79831032e8c84884
Sens3ii <densteeper@gmail.com>
2021-06-26 21:09:53 +0600

    Created site structure

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./app-header.css";
 
 const AppHeader = () => {
 	return (
 		<div className="app-header d-flex">
 			<h1>Daniil Koilybayev</h1>
+			<h2>Записей в ленте: 10 | Понравилось из них: 3</h2>
 		</div>
 	);
 };
