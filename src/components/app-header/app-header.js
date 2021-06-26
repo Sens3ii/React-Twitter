99d12022fd3c7279abcdfc90dd233ed25a4f1ef5
Sens3ii <densteeper@gmail.com>
2021-06-26 18:43:58 +0600

    Initial commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppHeader = () => {
+	return (
+		<div className="app-header d-flex">
+			<h1>Daniil Koilybayev</h1>
+		</div>
+	);
+};
+
+export default AppHeader;
