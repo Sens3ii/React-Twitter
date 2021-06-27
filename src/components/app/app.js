10d6e5efed4b99806aa4add2697c05c7cec3df6d
Sens3ii <densteeper@gmail.com>
2021-06-27 17:03:47 +0600

    delete post realization

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import PostStatusFilter from "../post-status-filter";
@@ -7,27 +7,47 @@ import PostAddForm from "../post-add-form";
 
 import "./app.css";
 
-const App = () => {
-	const data = [
-		{ label: "Hello guys this is my Twitter subscribe and like", important: true, id: "3feafeaf" },
-		{ label: "Yeah! Thanks guys I really appreciated your activity", id: "3fefefeaf" },
-		{ label: "OMG Someone hacked my account!!!! Noooo wayyyy", id: "3feaf43eaf" },
-		{ label: "Hmmmm wait a minute...", id: "3feaf55eaf" },
-		{ label: "Hey guys I'm nice", id: "3feafe65af" },
-		{ label: "Something wrong with my messages", id: "3fe87afeaf" },
-	];
-
-	return (
-		<div className="app">
-			<AppHeader />
-			<div className="search-panel d-flex">
-				<SearchPanel />
-				<PostStatusFilter />
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [
+				{ label: "Hello guys this is my Twitter subscribe and like", important: true, id: 1 },
+				{ label: "Yeah! Thanks guys I really appreciated your activity", id: 2 },
+				{ label: "OMG Someone hacked my account!!!! Noooo wayyyy", id: 3 },
+				{ label: "Hmmmm wait a minute...", id: 4 },
+				{ label: "Hey guys I'm nice", id: 5 },
+				{ label: "Something wrong with my messages", id: 6 },
+			],
+		};
+		this.maxId = 6;
+		this.deleteItem = this.deleteItem.bind(this);
+		this.addItem = this.addItem.bind(this);
+	}
+	deleteItem(id) {
+		this.setState(({ data }) => {
+			const newData = data.filter((elem) => elem.id !== id);
+			return { data: newData };
+		});
+	}
+	addItem(body) {
+		this.setState(({ data }) => {
+			const newData = [...data, { label: body, id: ++this.maxId }];
+			return { data: newData };
+		});
+	}
+	render() {
+		const { data } = this.state;
+		return (
+			<div className="app">
+				<AppHeader />
+				<div className="search-panel d-flex">
+					<SearchPanel />
+					<PostStatusFilter />
+				</div>
+				<PostList posts={data} onDelete={this.deleteItem} />
+				<PostAddForm onAdd={this.addItem} />
 			</div>
-			<PostList posts={data} />
-			<PostAddForm />
-		</div>
-	);
-};
-
-export default App;
+		);
+	}
+}
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,12 +2,17 @@ import React from "react";
 
 import PostListItem from "../post-list-item/";
 import "./post-list.css";
-const PostList = ({ posts }) => {
+const PostList = ({ posts, onDelete }) => {
 	const elements = posts.map((elem) => {
 		const { id, ...elemProps } = elem;
 		return (
 			<li key={id} className="list-group-item">
-				<PostListItem {...elemProps} />
+				<PostListItem
+					{...elemProps}
+					onDelete={() => {
+						onDelete(id);
+					}}
+				/>
 			</li>
 		);
 	});
